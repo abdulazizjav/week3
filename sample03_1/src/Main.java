88f2b50cfed00f859e78cf13dee73987c030ab93
Abdulaziz_java <rakhimoff1505@gmail.com>
2026-09-18 13:10:26 +0900

    Week 3 Java practice

---
+++ sample03_1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        boolean flag;
+        byte value1 =-128;
+        short value2 =32767;
+        int value3 =444444444;
+        long value4 = 4444444444444444L;
+        double value5 = 4.555555;
+        float value6 = 4.55555555f;
+    }
+}
