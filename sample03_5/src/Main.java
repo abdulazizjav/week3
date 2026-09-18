22c91be70844ec125451d7d494f7c3b5c02667c9
Abdulaziz_java <rakhimoff1505@gmail.com>
2026-09-18 13:52:06 +0900

    add new files

--- sample03_5/src/Main.java
+++ sample03_5/src/Main.java
@@ -5,23 +5,26 @@ public class Main {
 
         final double PI = 3.141592;
         double area;
-        double base;
+        int base;
+        int 사각형면적;
         double radius;
-        double squareArea;
-        double circleArea;
+        double 원의면적;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("정사각형의 한변의 길이 입력(예 5): ");
+        System.out.print("정사각형의 한변의 길이 입력(예 5) ");
         base = keyboard.nextInt();
 
-        squareArea = base * base;
+        사각형면적 = base * base;
         radius = base / 2.0;
-        circleArea = PI * radius * radius;
+        원의면적 = PI * radius * radius;
+        area = 사각형면적 - 원의면적;
 
-        area = squareArea - circleArea;
-
-        System.out.println("정사각형의 면적 - 원의 면적 = " + area);
+        System.out.printf("한 변의 길이가 %d Cm인 정사각형의 면적은 %d \u33a0%n",
+                base, 사각형면적);
+        System.out.printf("반지름이 %.2f Cm인 원의 면적은 %.2f \u33a0%n",
+                radius, 원의면적);
+        System.out.printf("구하는 면적 : %.2f \u33a0%n", area);
 
         keyboard.close();
     }
